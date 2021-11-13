3e68f85622493fc090b4ff5696844719dfa5df0e
codewxh <914067438@qq.com>
2021-11-13 22:16:34 +0800

    '配置初始化'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
 	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		<button type="default" @click="btn">点击</button>
 	</view>
 </template>
 
@@ -15,7 +12,58 @@
 			}
 		},
 		methods: {
-
+      btn(){
+				// 显示
+				// uni.showToast({
+				// 	title: "晚上好啊！",
+				// 	duration: 700
+				// 
+				
+				// uni.showLoading({
+				//     title: '加载中'
+				// });
+				
+				// setTimeout(function () {
+				//     uni.hideLoading(); // 隐藏消息提示框
+				// }, 2000);
+				
+				// 模态框
+				// uni.showModal({
+				//     title: '提示',
+				//     content: '这是一个模态弹窗',
+				//     success: function (res) {
+				//         if (res.confirm) {
+				//             console.log('用户点击确定');
+				//         } else if (res.cancel) {
+				//             console.log('用户点击取消');
+				//         }
+				//     }
+				// });
+				
+				// 从底部弹出
+				// uni.showActionSheet({
+				//     itemList: ['A', 'B', 'C'],
+				//     success: function (res) {
+				//         console.log('选中了第' + (res.tapIndex + 1) + '个按钮');
+				//     },
+				//     fail: function (res) {
+				//         console.log(res.errMsg);
+				//     }
+				// });
+				
+				// uni.setNavigationBarTitle({
+				//     title: '新的标题'
+				// });
+				uni.hideNavigationBarLoading()
+				uni.setNavigationBarColor({
+				    frontColor: '#ffffff',
+				    backgroundColor: '#ff0000',
+				    animation: {
+				        duration: 400,
+				        timingFunc: 'easeIn'
+				    }
+				})
+			}
 		}
 	}
 </script>
